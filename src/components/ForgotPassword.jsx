15dbc2b2891012d158d51967612e18a28ff4e278
edwin <edwin.macharia@gozem.co>
2023-10-21 08:24:30 +0300

    add reset password functions

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -7,31 +7,47 @@ import { toast } from 'react-toastify';
 const ForgotPassword = () => {
 
     const [userEmail, setUserEmail] = useState('')
-    const handleUserEmail = (e) => {
+    const [ response, setResponse ] = useState(null)
+    const [ code, setCode ] = useState('')
+    const [ newPassword, setNewPassword ] = useState('')
+    const handleEmailInput = (e) => {
         setUserEmail(e.target.value)
+    }
+    const handleCodeInput = (e) => {
+        setCode(e.target.value)
+    }
+    const handleNewPassword = (e) => {
+        setNewPassword(e.target.value)
     }
 
     const send_reset_email = async (e) => {
         e.preventDefault()
         console.log("user email", userEmail)
-
-        //check if user with email exists
-        const response = await Auth.signIn(userEmail);
-        console.log("response", response)
-        if (!response) {
-            toast.error("user does not exist")
-            return
-        }
-        console.log("user email", userEmail)
         try {
             const res = await Auth.forgotPassword(userEmail);
             console.log("forgot password response", res)
             toast.success("reset code sent")
+            setResponse(res)
         } catch (error) {
             console.log('error sending reset code', error);
             toast.error(error.message)
         }
     }
+    // handle password reset
+    const handle_password_reset = async (e) => {
+        e.preventDefault()
+        console.log("user email", userEmail)
+        try {
+            const res = await Auth.forgotPasswordSubmit(userEmail, code, newPassword);
+            console.log("forgot password response", res)
+            toast.success("password reset successful")
+            setResponse(res)
+        } catch (error) {
+            console.log('error sending reset code', error);
+            toast.error(error.message)
+        }
+    }
+
   return (
     <>
     <section class="sign-in">
@@ -46,10 +62,27 @@ const ForgotPassword = () => {
                         <form method="POST" class="register-form" id="login-form">
                             <div class="form-group">
                                 <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="email" value={userEmail} placeholder="Your Email" onChange={handleUserEmail}/>
+                                <input type="text" name="email" value={userEmail} placeholder="Your Email" onChange={handleEmailInput}/>
                             </div>
+                            {response && response.CodeDeliveryDetails && (
+                                <>
+                                                            <div class="form-group">
+                                                            <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                                            <input type="text" name="email" value={code} placeholder="Your Email" onChange={handleCodeInput}/>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                                            <input type="password" name="newPassword" placeholder="Enter New Password" value={newPassword} onChange={handleNewPassword}/>
+                                                        </div>
+                                                        </>
+                                                        )}
+
                             <div class="form-group form-button">
+                            {response && response.CodeDeliveryDetails ? (
+                                <input type="submit" onClick={handle_password_reset} class="form-submit" />
+                            ) : (
                                 <input type="submit" onClick={send_reset_email} class="form-submit" />
+                            )}
                             </div>
                         </form>
                     </div>
